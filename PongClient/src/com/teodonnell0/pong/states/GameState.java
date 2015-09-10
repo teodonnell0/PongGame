c0787f7ff51228fa3b99a0bb921b261a28e44e6f
Travis O'Donnell <teodonnell0@gmail.com>
2015-09-09 22:31:23 -0400

    Added a logger

--- PongClient/src/com/teodonnell0/pong/states/GameState.java
+++ PongClient/src/com/teodonnell0/pong/states/GameState.java
@@ -2,15 +2,22 @@ package com.teodonnell0.pong.states;
 
 import java.awt.Graphics2D;
 
+import org.apache.log4j.Logger;
+
+
 public abstract class GameState {
 
 	protected GameStateManager gameStateManager;
+	protected final Logger logger = Logger.getLogger(getClass());
 	
 	public GameState(GameStateManager gameStateManager) {
 		this.gameStateManager = gameStateManager;
 	}
 	
 	public abstract void init();
-	public abstract void update();
+	public void update() {
+		handleKeyInput();
+	}
+	public abstract void handleKeyInput();
 	public abstract void draw(Graphics2D graphics2D);
 }
